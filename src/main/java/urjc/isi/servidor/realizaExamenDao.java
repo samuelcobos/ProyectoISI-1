441087fffa28bf1c03ae33af39b23eed44285b6d
Samuel <47353257+samuelcobos@users.noreply.github.com>
2021-01-19 16:59:58 +0100

    Update realizaExamenDao.java

--- src/main/java/urjc/isi/servidor/realizaExamenDao.java
+++ src/main/java/urjc/isi/servidor/realizaExamenDao.java
@@ -20,7 +20,7 @@ public class realizaExamenDao {
             dbUri = new URI("https://proyect-isi.herokuapp.com/");
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + String.valueOf(dbUri.getPort()) + "/proyecto.db");
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + String.valueOf(dbUri.getPort()) + "/proyecto.db";
             
         	c = DriverManager.getConnection(dbUrl,username,password);
                 //c = DriverManager.getConnection("jdbc:postgresql:proyecto.db");
