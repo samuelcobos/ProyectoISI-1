cbf81f6e2cd55bca2ce46b103bdea3b1970e42ea
Samuel <47353257+samuelcobos@users.noreply.github.com>
2021-01-19 17:52:17 +0100

    Update alumnoDao.java

--- src/main/java/urjc/isi/servidor/alumnoDao.java
+++ src/main/java/urjc/isi/servidor/alumnoDao.java
@@ -28,7 +28,7 @@ public class alumnoDao {
             	c.setAutoCommit(false);
 
             c.prepareStatement("drop table if exists Alumnos").execute();
-            c.prepareStatement("CREATE TABLE "Alumnos" ("idAlumno"	VARCHAR(50) NOT NULL UNIQUE,"Nombre"	VARCHAR(50) NOT NULL,"Puerto"	INTEGER,"IP"	VARCHAR(50),PRIMARY KEY("idAlumno"))").execute();
+            c.prepareStatement("CREATE TABLE Alumnos (idAlumno	VARCHAR(50) NOT NULL UNIQUE,Nombre	VARCHAR(50) NOT NULL,Puerto	INTEGER,IP	VARCHAR(50),PRIMARY KEY(idAlumno))").execute();
 	
             	c.commit();
         } catch (SQLException e) {
